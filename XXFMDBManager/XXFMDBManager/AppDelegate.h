a038f0e49903f10d458eb2926074a54021c55af0
sunnyhigher <q562115005@outlook.com>
2018-04-18 10:18:36 +0800

    create

---
+++ XXFMDBManager/XXFMDBManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XXFMDBManager
+//
+//  Created by 段新瑞 on 2018/4/18.
+//  Copyright © 2018年 段新瑞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XXFMDBManager/XXFMDBManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXFMDBManager
+//
+//  Created by 段新瑞 on 2018/4/18.
+//  Copyright © 2018年 段新瑞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
